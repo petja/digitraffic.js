13b2af37644f7cbccf0d9c1c4050fc289ae6cb48
petja <hello@petja.me>
2019-12-21 08:15:56 +0200

    Use ESLint

---
+++ packages/rail/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard-with-typescript',
+  ],
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+  rules: {
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+  },
+}
